bb806aa9adf08ef4ac995bcfa370d130576e8197
MamakovAlekseyMikhailovich <itechmam@gmail.com>
2021-04-26 19:52:12 +0300

    fix: Исправления ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
-let openPopupButton = document.querySelector (".profile__button_edit");
-let popup = document.querySelector(".popup");
-let closePopupButton = document.querySelector(".popup__button_close");
-let formProfile = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input_name");
-let aboutInput = document.querySelector(".popup__input_about");
 let profileName = document.querySelector(".profile__title");
 let profileAbout = document.querySelector(".profile__subtitle");
+let openPopupButton = document.querySelector (".profile__edit-button");
 
+let popup = document.querySelector(".popup");
+let closePopupButton = document.querySelector(".popup__close");
+let formProfile = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_type_name");
+let aboutInput = document.querySelector(".popup__input_type_about");
 
-function togglePopupEditbutton(evt) {
-    evt.preventDefault();
-    popup.classList.toggle("popup_opened");
+function openPopup () {
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
+    popup.classList.add ("popup_opened");
 }
 
-function editProfile(evt) {
-    evt.preventDefault();
+function closePopup () {
+    popup.classList.remove ("popup_opened");
+}
+  
+function submitPopup (event) {
+    event.preventDefault();
+  
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
-    togglePopupEditbutton(evt);
+    closePopup();
 }
 
-openPopupButton.addEventListener("click", togglePopupEditbutton);
-closePopupButton.addEventListener("click", togglePopupEditbutton);
-formProfile.addEventListener("submit", editProfile);
+openPopupButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+formProfile.addEventListener("submit", submitPopup);
